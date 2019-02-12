aad8fc06666907bcc533a480840963996da1f8b7
Tets5441 <tetley640@gmail.com>
2019-02-12 11:39:26 +0000

    resolved checkwin issues
    this now registers the correct guess and resets appropiately

--- app.js
+++ app.js
@@ -1,106 +1,110 @@
+// Global variables
+
 const qwerty = document.getElementById('qwerty');
 let phrase = document.getElementById('phrase');
-const startBtn = document.querySelector('.btn__reset');
+const startBtn= document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
-const UL = document.querySelector('ul');
-const title = document.createElement('title');
-const letter = document.getElementsByClassName('letter');
-const space = document.getElementsByClassName('space');
-const mainContainer = document.querySelector('.main-container');
-const div = document.createElement('div');
+const title = document.querySelector('.title');
+const button = document.querySelector('.btn__reset');
 const keyboardButton = qwerty.querySelectorAll('button');
-const playAgainButton = document.createElement('button');
-const scoreBoard = document.getElementById('scoreboard');
-var scoreboardList = scoreBoard.children;
+const heart = document.getElementsByTagName('img');
+const UL = document.querySelector('ul');
 let missed = 0;
-let lostHeart = 5;
 
+// create array
+const phrases = ['iron maiden', 'machine head', 'faith no more', 'mad caddies', 'pennywise'];
 
 //hide overlay when start button clicked
 startBtn.addEventListener('click', (event) => {
-    overlay.style.display = 'none';
-})
-
-// create array
-const phrases = ['IRON MAIDEN', 'MACHINE HEAD', 'FAITH NO MORE', 'MAD CADDIES', 'PENNYWISE'];
+  overlay.style.display = 'none';
+});
 
 //do stuff to any arr that is passed in 
 const getRandomPhraseArray = arr => {
-    return phrase = arr[Math.floor(Math.random() * arr.length)]; 
+  return phrase = arr[Math.floor(Math.random() * arr.length)]; 
 }
+//random phrase split array
 
-// set phrase
+
+//Set the game display
 const addPhraseToDisplay = arr => {
-    for(let i = 0; i < arr.length; i += 1) {
-        let li = document.createElement('li');
-        li.textContent = arr[i].toUpperCase();
-        UL.appendChild(li);
-        if (arr[i] != ' ') {
-            li.className = 'letter';
-        } else {
-            li.className = 'space';
-        }
-    }
+  for(let i = 0; i < arr.length; i += 1) {
+      let li = document.createElement('li');
+      li.textContent = arr[i];
+      UL.appendChild(li);
+      if (arr[i] != ' ') {
+          li.className = 'letter';
+      } else {
+          li.className = 'space';
+      }
+  }
 }
 
 const phraseArray = getRandomPhraseArray(phrases);
 addPhraseToDisplay(phraseArray);
 
-// add event listener to keyboard
-
-qwerty.addEventListener("click", (e) => {
-  if (e.target.tagName === "BUTTON") {
-    const button = e.target;
-    button.classList.add("chosen");
-    button.disabled = "true";
-    const checked = checkLetter(button);
-      if (checked === null) {
-        missed += 1;
-        console.log("That's! " + missed + " incorrect guess(s).");
-        document.querySelector(".tries").className = "lostheart";
-      }
-      checkWin();
-  }
-});
-
-// check letter
-//this is not working, does not show as correct when  correct letter is picked
+// check if guessed letter is correct
 
 const checkLetter = yourGuess => {
-    let li = document.querySelectorAll('.letter');
-    let letterFound;
-    for (let i = 0; i < li.length; i++) {
-      const letter = li[i];
-      if (yourGuess.textContent == letter.textContent) {
-      letter.classList.add("show");
-      letterFound = letter;
+  const letters = document.querySelectorAll('.letter');
+  let letterFound = null;
+  for(let i = 0; i < letters.length; i+= 1) {
+    if (letters[i].textContent === yourGuess ) {
+      letters[i].classList.add('show');
+      letterFound = true;
     }
-  }
-    if (letterFound) {
-      console.log("That's correct!");
-      return letterFound;
-  } else {
-      return null;
-  }
+  } return letterFound
 }
 
-  function checkWin() {
-    const liLetter = document.querySelectorAll(".letter");
-    const liShow = document.querySelectorAll(".show");
-    if (liLetter.length === liShow.length) {
-      playAgain();
-      title.textContent = "You win";
-      div.id = "win";
-      div.classList.add("win");
-      console.log("You win!");
-    }
-    if (missed == 5) {
-      playAgain();
-      title.textContent = "Sorry, you didn't win!";
-      div.id = "lose";
-      div.classList.add("lose");
-      console.log("Sorry, you didn't win!");
-    }
+// Check win or lose
+
+function checkWin() {
+  let show = document.querySelectorAll('.show');
+  let letter = document.querySelectorAll('.letter');
+  let next = getRandomPhraseArray(phrases);
+  if (show.length === letter.length ) {
+    overlay.className = 'win';
+    overlay.style.display = 'flex';
+    title.textContent = 'Well done, you won!'
+    button.textContent = 'Play again?';
+  } else if (missed === 5) {
+    overlay.className = 'lose';
+    overlay.style.display = 'flex';
+    title.textContent = 'Oops you lost!'
+    button.textContent = 'Play again?';
   }
 
-  
+    startBtn.addEventListener('click', (event) => {
+      missed = 0;
+      UL.textContent = '';
+      overlay.style.display = 'none';
+      for (let i = 0; i < heart.length; i+= 1) {
+        heart[i].src = 'images/liveHeart.png';
+      }
+      for (i = 0; i < keyboardButton.length; i++) {
+        keyboardButton[i].removeAttribute('class');
+        keyboardButton[i].removeAttribute('disabled');
+      }
+      addPhraseToDisplay(next);
+    });
+  //}
+}
+
+// add event listener to keyboard
+
+qwerty.addEventListener('click', (event) => {
+  let target = event.target;
+  const button = target.textContent;
+  if (target.type === 'submit') {
+    target.classList.add('chosen');
+    target.setAttribute('disabled', ' ');
+    let letterFound = checkLetter(button);
+    if (letterFound === null) {
+    for (let i = 0; i < heart.length; i++) {
+      heart[missed].src = 'images/lostHeart.png';
+      }
+    missed += 1;
+   }
+  }
+  checkWin();
+});
